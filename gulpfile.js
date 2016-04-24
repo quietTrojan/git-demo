a366ddb4b03f399a5e8806391f8da4e5e36ef51d
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-04-24 10:25:28 +0800

    xiugai

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,39 @@ var gulp = require('gulp'), //本地安装gulp所用到的地方
     concat = require('gulp-concat'),
     fileinclude = require('gulp-file-include'),
     styleInject = require("gulp-style-inject");
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 //定义一个testLess任务（自定义任务名称）
-gulp.task('less_css', function () {
-    return gulp.src(['src/less/*.less','!src/less/reuseClass.less']) //该任务针对的文件
+/*gulp.task('less_css', function () {
+    return gulp.src(['src/less/!*.less','!src/less/reuseClass.less']) //该任务针对的文件
         .pipe(less())
         .pipe(gulp.dest('dist/css/')); //将会在src/css下生成index.css
     console.log('less编译',new Date().getTime());
+});*/
+
+gulp.task('sass_css', function () {
+    return gulp.src(['src/sass/*.scss']) //该任务针对的文件
+        .pipe(sass().on('error', sass.logError))
+        .pipe(prefix())
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist/css/')); //将会在src/css下生成index.css
+    console.log('sass编译',new Date().getTime());
 });
 
-gulp.task('default', ['less_css']);
+gulp.task('imagemin', function(){
+    return gulp.src('src/images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('default', ['sass_css']);
 //gulp.task('default', ['concat_js','less_css','file_include']);
 
  /*gulp.task('styleInject',['less_css'],function () {
